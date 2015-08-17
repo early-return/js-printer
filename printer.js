fa21bd193df5ea0f0ecdc3643aa383d66e3886b3
Zhiqing <lizhiqing1996@gmail.com>
2015-08-17 08:28:10 +0800

    Update printer.js

--- printer.js
+++ printer.js
@@ -1,20 +1,20 @@
 var printer = new Object();
 
 printer.str = 'hello,world!';   //要实现打字机效果的文字
-printer.toId = 'body';		//要打印到的标签的ID
-printer.toObj;			
+printer.speed = 50;		//文字的速度
+printer.toId = 'body';		//要打印到的标签的ID			
 printer.startIndex = 0		//从第几个字符开始打印
-printer.index = 0;
 printer.endIndex = 0;		//打印到第几个字符结束
 printer.hasCur = true;		//是否有光标
 printer.curId = 'cur';		//光标的ID
 printer.curStr = '_';		//光标字符
 printer.curStyle = 'font-weight: bold;';	//光标的样式（CSS样式）
-printer.curObj;
 printer.curSpeed = 500;		//光标的速度（ms）
+printer.index = 0;
+printer.toObj;
 printer.curSwitch = false;
+printer.curObj;
 printer.reStr = '';
-printer.speed = 50;		//文字的速度
 printer.flwCurTimer;
 
 printer.init = function(){	//初始化一些内容
